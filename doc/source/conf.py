a87a1db041fd28bfc75b85f7bed3b07e2e5bd280
James E. Blair <jeblair@redhat.com>
2018-05-04 14:50:13 -0700

    Add logo to docs
    Change-Id: Ic39e19e4adca85ecf8d67b35de51d604ec2510be

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -69,3 +69,17 @@ latex_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 # intersphinx_mapping = {'http://docs.python.org/': None}
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = '_static/logo.svg'
+
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+#html_favicon = None
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
